a608eea5d01c592f4d2191c4a4db68fa6e770b2c
sush104 <sushant.shelke104@gmail.com>
2023-02-02 12:59:29 +0000

    React To Do app with ReactFlip

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NavBar from './Components/navBar';
+import Footer from './Components/footer';
+ 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+ 
+export default App;
