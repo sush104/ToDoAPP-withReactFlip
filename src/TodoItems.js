ca69f3a00f5ff351a5005c9f6ce51a46566566df
Sushant Shelke <43604669+sush104@users.noreply.github.com>
2026-03-01 17:08:47 +0000

    Refactor TodoItems to include item text in delete

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,32 +1,35 @@
 import React, { Component } from "react";
 import FlipMove from "react-flip-move";
- 
+
 class TodoItems extends Component {
-    constructor(props) {
-        super(props);
-     
-        this.createTasks = this.createTasks.bind(this);
-      }
-      delete(key) {
-        this.props.delete(key);
-      }
-    createTasks(item) {
-        return <li onClick={() => this.delete(item.key)} 
-                    key={item.key}>{item.text}</li>
-      }
- 
+  constructor(props) {
+    super(props);
+
+    this.createTasks = this.createTasks.bind(this);
+  }
+  delete(key, text) {
+    this.props.delete(key, text);
+  }
+  createTasks(item) {
+    return (
+      <li onClick={() => this.delete(item.key, item.text)} key={item.key}>
+        {item.text}
+      </li>
+    );
+  }
+
   render() {
     var todoEntries = this.props.entries;
     var listItems = todoEntries.map(this.createTasks);
- 
+
     return (
-        <ul className="theList">
+      <ul className="theList">
         <FlipMove duration={500} easing="ease-out">
           {listItems}
         </FlipMove>
       </ul>
     );
   }
-};
- 
+}
+
 export default TodoItems;
